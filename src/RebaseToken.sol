a48f92df44f8a0d5650387adfec3357a740479ca
Vib-UX <vibchess123@gmail.com>
2025-03-18 22:17:57 +0530

    adds accrued-mint logic

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -15,6 +15,8 @@ contract RebaseToken is ERC20 {
     error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
 
     uint256 private s_interestRate = 5e10;
+    mapping(address => uint256) private s_userInterestRate;
+    mapping(address => uint256) private s_userLastUpdatedTimestamp;
 
     event InterestRateSet(uint256 interestRate);
 
@@ -31,5 +33,37 @@ contract RebaseToken is ERC20 {
         }
         s_interestRate = _newInterestRate;
         emit InterestRateSet(_newInterestRate);
+    }
+
+    function mint(address _to, uint256 _amount) external {
+        _mintAccruedInterest(_to);
+        s_userInterestRate[_to] = s_interestRate;
+        _mint(_to, _amount);
+    }
+
+    function balanceOf(address _account) public view override returns (uint256) {
+        // get the current principle balance of the user that is
+        uint256 _interest = (block.timestamp - s_userLastUpdatedTimestamp[_account]) * s_userInterestRate[_account];
+        return super.balanceOf(_account) + _interest;
+    }
+
+    function _mintAccruedInterest(address _user) internal {
+        // (1) find the current balance of rebase tokens that have been minted to the user
+        // (2) calculate their current balance including any interest -> balanceOf
+        // calculate the number of tokens that need to be mitned to the user -> (2) -(1)
+        // call the _mint to mint the tokens to the user
+        // set the user last updated timestamp
+        uint256 _interest = (block.timestamp - s_userLastUpdatedTimestamp[_user]) * s_userInterestRate[_user];
+        s_userLastUpdatedTimestamp[_user] = block.timestamp;
+        _mint(_user, _interest);
+    }
+
+    /*
+    * @notice Get the interest rate for the user
+    * @param _user The user for which the interest rate is to be fetched
+    * @return The interest rate for the user
+    */
+    function getUserInterestRate(address _user) external view returns (uint256) {
+        return s_userInterestRate[_user];
     }
 }
